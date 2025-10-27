e2d80abc70ea88182e49c68c6154b76184476c5e
CarinaMendes01 <projetogafanhotosetec@hotmail.com>
2025-10-27 12:55:05 -0300

    .

--- api/componentes.js
+++ api/componentes.js
@@ -4,9 +4,9 @@
 async function carregarComponente(id, arquivo) {
   try {
     const resp = await fetch(arquivo);
-    if (!resp.ok) throw new Error(`Erro ao carregar ${arquivo}`);
-    const html = await resp.text();
-    document.getElementById(id).innerHTML = html;
+    // if (!resp.ok) throw new Error(`Erro ao carregar ${arquivo}`);
+    // const html = await resp.text();
+    // document.getElementById(id).innerHTML = html;
   } catch (erro) {
     console.error("Falha ao importar componente:", erro);
   }
