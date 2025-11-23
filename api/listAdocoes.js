ebff96e849ce3acf113688f77d837dea7e99169c
CarinaMendes01 <projetogafanhotosetec@hotmail.com>
2025-11-23 13:39:31 -0300

    .

--- api/listAdocoes.js
+++ api/listAdocoes.js
@@ -183,7 +183,7 @@ export default async function handler(req, res) {
       try {
         const movRecords = await base("ponto_movimentos")
           .select({
-            filterByFormula: `FIND('${r.id}', ARRAYJOIN({adocoes}))`,
+            filterByFormula: `{id_doacao (from adocoes)} = '${f.id_doacao}'`,
             sort: [{ field: "id_movimentacao_ponto", direction: "asc" }],
           })
           .all();
@@ -197,6 +197,7 @@ export default async function handler(req, res) {
       } catch (e) {
         console.log("Erro ao buscar movimentos do ponto:", e);
       }
+
 
 
       // ======================================================
