653f602c89cca8ca0bce3d50ff1b246682bc8ca6
CarinaMendes01 <projetogafanhotosetec@hotmail.com>
2025-11-24 18:49:15 -0300

    campo obsercaçoes api listAdocoes js

--- api/listAdocoes.js
+++ api/listAdocoes.js
@@ -183,7 +183,8 @@ export default async function handler(req, res) {
       try {
         const movRecords = await base("ponto_movimentos")
           .select({
-            filterByFormula: `FIND('${r.id}', ARRAYJOIN({adocoes}))`,
+            filterByFormula: `FIND('${f.id_doacao}', ARRAYJOIN({adocoes}))`,
+
             sort: [{ field: "id_movimentacao_ponto", direction: "asc" }],
           })
           .all();
